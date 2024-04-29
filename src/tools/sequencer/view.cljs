ab179d28e4975c6e5bb22bdd9e32be3b407cfa2e
Sunny <embodiedsunshine@gmail.com>
2024-04-29 13:40:01 -0600

    removed old unused component

--- src/tools/sequencer/view.cljs
+++ src/tools/sequencer/view.cljs
@@ -14,22 +14,3 @@
 ;;   less than or equal to the length of the sequence from min to max
 ;; dups must be greater than 0 and less than or equal to the length of the sequence
 
-(defn sequencer []
-  (let [nums (r/atom nil)
-        options (r/atom {:min 4 :max 12 :len 6 :dups 1})]
-    (fn []
-      [:div
-       (sx :.full-rounded
-           :bgc--white
-           :p--1.5rem)
-       [:div
-        "sequencer"]
-       [:pre (str/join ", " @nums)]
-       [:button
-        (sx :mb--1rem
-            :.full-rounded
-            :.filled
-            :.positive
-            :p--0.5rem
-            {:on-click (fn [_] (reset! nums (generate! @options)))})
-        "Generate"]])))
